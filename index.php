9914ede9e3f15255eac7fb14f9c433b2d6bf4559
qp <qp@powerme.com.sg>
2015-09-11 05:41:31 +0800

    first push

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>High Fashion Website Template</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<!--[if IE 7]>
+		<link rel="stylesheet" href="css/ie7.css" type="text/css">
+	<![endif]-->
+</head>
+<body>
+	<div class="header">
+		<div>
+			<a href="index.html" id="logo"><img src="images/singterpret.png" alt="logo"></a>
+			<ul>
+				<li class="selected">
+					<a href="index.php">Home</a>
+				</li>
+				<li>
+					<a href="#">About Us</a>
+				</li>
+				<li>
+					<a href="#">More</a>
+					<ul>
+						<li>
+							<a href="#">Contact Us</a>
+						</li>
+						<li>
+							<a href="#">Our Blog</a>
+						</li>
+						<li>
+							<a href="#">Careers</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div class="featured">
+			<img src="images/bg-absolute.png" alt="">
+			<div>
+				<h3>Less... <br> Is More...</h3>
+				<a href="collections.html">2023 Spring Collection</a>
+			</div>
+		</div>
+	</div>
+	
+	<div class="footer">
+		<div>
+			<p>
+				&#169; Singterpret
+			</p>
+			<ul>
+				<li>
+					<a href="#">Privacy Policy</a>
+				</li>
+				<li>
+					<a href="#">Terms And Conditons</a>
+				</li>
+			</ul>
+			<div class="connect">
+				<a href="#" id="twitter">Twitter</a> <a href="#" id="facebook">Facebook</a> <a href="#" id="googleplus">Google+</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
